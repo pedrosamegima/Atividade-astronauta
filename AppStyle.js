82cd664fe61887e74e7570dacbe119fe44fd726d
pedrosamegima <phsamegima@gmail.com>
2024-03-26 16:37:59 -0300

    Exercicio astronauta

---
+++ AppStyle.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'black',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    imagens:{
+        marginTop:-100,
+        height:650,
+    },
+    texto:{
+        textAlign: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: 'white',
+        fontSize: 20,
+    },
+
+
+  });
+  
+
